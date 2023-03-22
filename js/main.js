be4f9701ab6dbf137a93d0e2dc9267d61278878a
DmSeptim <100709645+DmSeptim@users.noreply.github.com>
2023-03-22 15:11:23 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,17 +11,27 @@ window.onscroll = () =>{
         header.classList.remove("header_active")
     }
 } 
-}())
+}());
 
 
 (function () {
     const burgerMenu = document.querySelector(".header-burger");
     const menu = document.querySelector(".header-nav")
     const menuClose =document.querySelector('.heder-burger-close')
+    const menuCloseLink = document.querySelectorAll(".header-link")
    burgerMenu.addEventListener("click", ()=> {
     menu.classList.add("header-nav-active")
    })
    menuClose.addEventListener("click", () => {
     menu.classList.remove("header-nav-active")
    } )
+   if(window.innerWidth < 768){
+    for(let i = 0; i < menuCloseLink.length; i++){
+        menuCloseLink[i].addEventListener('click', () =>{
+            menu.classList.remove("header-nav-active")
+        })
+    }
+   }
 }())
+
+
